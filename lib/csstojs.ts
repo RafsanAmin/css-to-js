98127d36e46036cd7c51f2e95149ed7c16626f5d
RafsanAmin <rafsanamin2020@gmail.com>
2021-12-03 10:34:46 +0600

    update

--- lib/csstojs.ts
+++ lib/csstojs.ts
@@ -2,21 +2,18 @@ const cssToJs = (param: string) => {
   const lines = param
     .replace(/{}/g, '')
     .split(';')
-    .filter((s) => s);
+    .filter((s) => s.match(/^([\w\-]+:[+-=-*!@#$%^&*0-9,.()"'\-A-Za-z ]+)$/g));
   const ret: string[] = [];
   console.log(lines);
   for (let a of lines) {
-    if (a.match(/^([\w\-]+:[+-=-*!@#$%^&*0-9,.()"'\-A-Za-z ]+)$/g)) {
-      const css: string[] = a.split(/:[ ]*/g);
-      const propertyName = css[0];
-      const property = `'${css[1].replace(/'/g, '"')}'`;
-      const newPropertyName = propertyName.replace(/\-[A-Za-z]/g, (str) =>
-        str.replace('-', '').toUpperCase()
-      );
+    const css: string[] = a.split(/:[ ]*/g);
+    const propertyName = css[0];
+    const property = `'${css[1].replace(/'/g, '"')}'`;
+    const newPropertyName = propertyName.replace(/\-[A-Za-z]/g, (str) =>
+      str.replace('-', '').toUpperCase()
+    );
 
-      ret.push(newPropertyName + ': ' + property);
-    } else {
-    }
+    ret.push(newPropertyName + ': ' + property);
   }
   console.log(ret.join(',\n'));
   return ret.join(',\n');
